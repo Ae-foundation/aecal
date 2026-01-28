538cb9f8e91b7a148b30bbdda853b622e89b7f9a
lomaster <119946584+Lomasterrrr@users.noreply.github.com>
2026-01-28 03:57:48 +0300

    Update config.h

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define __AECAL_CONFIG_H
 
 #define CURRENT_DAY_FLAG 1		/* highlight current day? */
-#define ANSI_HIGHLIGHT "[1;5;41;37m"	/* ansi code for hightligh */
+#define ANSI_HIGHLIGHT "[1;5;41;37m"	/* ansi code for hightlight */
 #define MONDAY_FLAG 1			/* first week day is monday? */
 
 #endif
